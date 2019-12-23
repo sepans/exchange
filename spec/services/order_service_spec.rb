e653de2d826423340768b62d4b55c9d35ea99190
sepans <sepans@sepans.com>
2019-12-23 15:57:40 -0500

    Add refund! reject! specs

--- spec/services/order_service_spec.rb
+++ spec/services/order_service_spec.rb
@@ -306,4 +306,266 @@ describe OrderService, type: :services do
       end
     end
   end
+
+  describe '#reject!' do
+    let(:state) { Order::SUBMITTED }
+    let(:order_mode) { Order::BUY }
+
+    context 'with a successful payment intent cancel' do
+      before do
+        prepare_payment_intent_cancel_success
+        OrderService.reject!(order, user_id)
+        # transaction = order.transactions.order(created_at: :desc).first
+        # service.reject!
+      end
+
+      it 'queues undeduct inventory job for buy now order' do
+        expect(UndeductLineItemInventoryJob).to have_been_enqueued.with(line_items.first.id)
+      end
+
+      it 'records the transaction' do
+        transaction = order.transactions.order(created_at: :desc).first
+        expect(transaction).to have_attributes(external_id: 'test_ch_3', transaction_type: Transaction::CANCEL, status: Transaction::SUCCESS)
+      end
+
+      it 'updates the order state' do
+        expect(order.state).to eq Order::CANCELED
+        expect(order.state_reason).to eq Order::REASONS[Order::CANCELED][:seller_rejected_other]
+      end
+
+      it 'queues notification job' do
+        expect(PostEventJob).to have_been_enqueued.with('commerce', kind_of(String), 'order.canceled')
+      end
+
+      context 'offer order' do
+        let(:order_mode) { Order::OFFER }
+        let!(:line_items) { [Fabricate(:line_item, order: order, artwork_id: 'a-1', list_price_cents: 123_00)] }
+        it 'does not queue undeduct inventory job for make offer order' do
+          expect(UndeductLineItemInventoryJob).not_to have_been_enqueued
+        end
+      end
+    end
+
+    context 'with an unsuccessful payment intent cancelation' do
+      before do
+        prepare_payment_intent_cancel_failure(charge_error: { code: 'something', message: 'refund failed', decline_code: 'failed_refund' })
+        expect { OrderService.reject!(order, user_id) }.to raise_error(Errors::ProcessingError).and change(order.transactions, :count).by(1)
+      end
+
+      it 'raises a ProcessingError and records the transaction' do
+        transaction = order.transactions.order(created_at: :desc).first
+        expect(transaction).to have_attributes(external_id: 'pi_1', transaction_type: Transaction::CANCEL, status: Transaction::FAILURE)
+      end
+
+      it 'does not queue undeduct inventory job' do
+        expect(UndeductLineItemInventoryJob).not_to have_been_enqueued.with(line_items.first.id)
+      end
+    end
+
+    context 'when the order is paid for by wire transfer' do
+      it 'raises a ValidationError' do
+        order.update!(payment_method: Order::WIRE_TRANSFER)
+        expect { OrderService.reject!(order, user_id) }.to raise_error do |e|
+          expect(e.code).to eq :unsupported_payment_method
+        end
+      end
+    end
+
+    context 'with an offer-mode order' do
+      let!(:offer) { Fabricate(:offer, order: order, from_id: order.buyer_id, from_type: order.buyer_type) }
+      # let(:service) { OrderCancellationService.new(order, 'seller') }
+
+      before do
+        # last_offer is set in Orders::InitialOffer. "Stubbing" out the
+        # dependent behavior of this class to by setting last_offer directly
+        order.update!(last_offer: offer, mode: 'offer')
+      end
+
+      describe 'with a submitted offer' do
+        it 'updates the state of the order' do
+          expect do
+            OrderService.reject!(order, user_id, Order::REASONS[Order::CANCELED][:seller_rejected_offer_too_low])
+          end.to change { order.state }.from(Order::SUBMITTED).to(Order::CANCELED)
+                                       .and change { order.state_reason }
+            .from(nil).to(Order::REASONS[Order::CANCELED][:seller_rejected_offer_too_low])
+        end
+
+        it 'instruments an rejected offer' do
+          dd_statsd = stub_ddstatsd_instance
+          expect(dd_statsd).to receive(:increment).with('order.reject')
+          OrderService.reject!(order, user_id, Order::REASONS[Order::CANCELED][:seller_rejected_offer_too_low])
+        end
+
+        it 'sends a notification' do
+          expect(PostEventJob).to receive(:perform_later).with('commerce', kind_of(String), 'order.canceled')
+          OrderService.reject!(order, user_id, Order::REASONS[Order::CANCELED][:seller_rejected_offer_too_low])
+        end
+      end
+
+      describe "when we can't reject the order" do
+        let(:order) { Fabricate(:order, state: Order::PENDING) }
+        let(:offer) { Fabricate(:offer, order: order) }
+
+        it "doesn't instrument" do
+          dd_statsd = stub_ddstatsd_instance
+          allow(dd_statsd).to receive(:increment).with('offer.reject')
+
+          expect { OrderService.reject!(order, user_id, Order::REASONS[Order::CANCELED][:seller_rejected_offer_too_low]) }.to raise_error(Errors::ValidationError)
+
+          expect(dd_statsd).to_not have_received(:increment)
+        end
+      end
+    end
+  end
+
+  xdescribe '#seller_lapse!' do
+    context 'Buy Order' do
+      context 'with a successful payment intent cancelation' do
+        before do
+          prepare_payment_intent_cancel_success
+          service.seller_lapse!
+        end
+
+        it 'queues undeduct inventory job' do
+          expect(UndeductLineItemInventoryJob).to have_been_enqueued.with(line_items.first.id)
+        end
+
+        it 'records the transaction' do
+          transaction = order.transactions.order(created_at: :desc).first
+          expect(transaction).to have_attributes(external_id: 'pi_1', external_type: Transaction::PAYMENT_INTENT, transaction_type: Transaction::CANCEL, status: Transaction::SUCCESS)
+        end
+
+        it 'updates the order state' do
+          expect(order.state).to eq Order::CANCELED
+          expect(order.state_reason).to eq Order::REASONS[Order::CANCELED][:seller_lapsed]
+        end
+
+        it 'queues notification job' do
+          expect(PostEventJob).to have_been_enqueued.with('commerce', kind_of(String), 'order.canceled')
+        end
+      end
+
+      context 'with an unsuccessful payment intent cancelation' do
+        before do
+          prepare_payment_intent_cancel_failure(charge_error: { code: 'something', message: 'refund failed', decline_code: 'failed_refund' })
+          expect { OrderService.reject!(order, user_id) }.to raise_error(Errors::ProcessingError).and change(order.transactions, :count).by(1)
+        end
+
+        it 'raises a ProcessingError and records the transaction' do
+          transaction = order.transactions.order(created_at: :desc).first
+          expect(transaction).to have_attributes(external_id: 'pi_1', transaction_type: Transaction::CANCEL, status: Transaction::FAILURE)
+        end
+
+        it 'does not queue undeduct inventory job' do
+          expect(UndeductLineItemInventoryJob).not_to have_been_enqueued.with(line_items.first.id)
+        end
+      end
+    end
+
+    context 'Offer Order' do
+      let(:order_mode) { Order::OFFER }
+      let!(:line_items) { [Fabricate(:line_item, order: order, artwork_id: 'a-1', list_price_cents: 123_00)] }
+
+      before do
+        prepare_payment_intent_refund_success
+        service.seller_lapse!
+      end
+
+      it 'updates the order state' do
+        expect(order.state).to eq Order::CANCELED
+        expect(order.state_reason).to eq Order::REASONS[Order::CANCELED][:seller_lapsed]
+      end
+
+      it 'queues notification job' do
+        expect(PostEventJob).to have_been_enqueued.with('commerce', kind_of(String), 'order.canceled')
+      end
+    end
+  end
+
+  xdescribe '#buyer_lapse!' do
+    context 'Offer Order' do
+      let(:order_mode) { Order::OFFER }
+      let!(:line_items) { [Fabricate(:line_item, order: order, artwork_id: 'a-1', list_price_cents: 123_00)] }
+
+      before do
+        prepare_payment_intent_refund_success
+        service.buyer_lapse!
+      end
+
+      it 'updates the order state' do
+        expect(order.state).to eq Order::CANCELED
+        expect(order.state_reason).to eq Order::REASONS[Order::CANCELED][:buyer_lapsed]
+      end
+
+      it 'queues notification job' do
+        expect(PostEventJob).to have_been_enqueued.with('commerce', kind_of(String), 'order.canceled')
+      end
+    end
+  end
+
+  describe '#refund!' do
+    [Order::APPROVED, Order::FULFILLED].each do |state|
+      context "#{state} order" do
+        let(:state) { state }
+
+        context 'with a successful refund' do
+          before do
+            prepare_payment_intent_refund_success
+            Fabricate(:transaction, order: order, external_id: 'test_ch_3', external_type: Transaction::PAYMENT_INTENT)
+            OrderService.refund!(order, user_id)
+          end
+
+          it 'queues undeduct inventory job' do
+            expect(UndeductLineItemInventoryJob).to have_been_enqueued.with(line_items.first.id)
+          end
+
+          it 'records the transaction' do
+            transaction = order.transactions.order(created_at: :desc).first
+            expect(transaction).to have_attributes(external_id: 're_1', transaction_type: Transaction::REFUND, status: Transaction::SUCCESS)
+          end
+
+          it 'updates the order state' do
+            expect(order.state).to eq Order::REFUNDED
+          end
+
+          it 'queues notification job' do
+            expect(PostEventJob).to have_been_enqueued.with('commerce', kind_of(String), 'order.refunded')
+          end
+        end
+
+        context 'with an unsuccessful refund' do
+          before do
+            Fabricate(:transaction, order: order, external_id: 'test_ch_3', external_type: Transaction::PAYMENT_INTENT)
+            prepare_payment_intent_refund_failure(code: 'something', message: 'refund failed', decline_code: 'failed_refund')
+            expect {  OrderService.refund!(order, user_id) }.to raise_error(Errors::ProcessingError).and change(order.transactions, :count).by(1)
+          end
+
+          it 'raises a ProcessingError and records the transaction' do
+            transaction = order.transactions.order(created_at: :desc).first
+            expect(transaction).to have_attributes(external_id: 'pi_1', transaction_type: Transaction::REFUND, status: Transaction::FAILURE)
+          end
+
+          it 'does not queue undeduct inventory job' do
+            expect(UndeductLineItemInventoryJob).not_to have_been_enqueued.with(line_items.first.id)
+          end
+        end
+
+        context 'when the order is paid for by wire transfer' do
+          it 'raises a ValidationError' do
+            order.update!(payment_method: Order::WIRE_TRANSFER)
+            expect { service.refund! }.to raise_error do |e|
+              expect(e.code).to eq :unsupported_payment_method
+            end
+          end
+        end
+      end
+    end
+  end
+
+  def stub_ddstatsd_instance
+    dd_statsd = double(Datadog::Statsd)
+    allow(Exchange).to receive(:dogstatsd).and_return(dd_statsd)
+
+    dd_statsd
+  end  
 end
